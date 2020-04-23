53802d24fabfcdaff18fb9127e47a272adaa905c
naeiougel <nigelabigania@gmail.com>
2020-04-23 11:43:40 -0700

    removed commented out package

--- wher_da_switches_at.py
+++ wher_da_switches_at.py
@@ -5,7 +5,7 @@ import os
 import json
 import pandas as pd
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 
